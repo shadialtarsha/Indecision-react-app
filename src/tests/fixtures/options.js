ec60997aafe8a76ee4e3eb4395a9d6d7acd64388
shadialtarsha <shadi.altarsha.94@gmail.com>
2018-03-31 09:20:38 +0300

    fix tests

--- src/tests/fixtures/options.js
+++ src/tests/fixtures/options.js
@@ -1,1 +1,14 @@
-export default ['Buy a car', 'Go Shopping', 'Swimming'];
+export default [
+  {
+    id: '1',
+    text: 'Buy a car',
+  },
+  {
+    id: '2',
+    text: 'Go Shopping',
+  },
+  {
+    id: '3',
+    text: 'Swimming',
+  },
+];
